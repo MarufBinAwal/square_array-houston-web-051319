fe40fe543811141d9250f52a038fa0a9cf940e8a
MARUF B AWAL <marufbinawal@gmail.com>
2019-05-11 17:59:18 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,4 @@
-def square_array(["apple 1","apple 2","apple 3","apple 4","apple 5","apple 6","apple 7","apple 8","apple 9"])
+def square_array([1,2,3,4,5])
   # your code here
   
   square_array.each do |
